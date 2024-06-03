2021adc2717e12ccb3753e02f17512e1f083b16c
Sacha Arseneault <sacha.arseneault@gmail.com>
2024-06-03 14:13:46 -0400

    feat: project setup

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Navigation from "./components/Navigation";
+import Hero from "./components/Hero";
+import Services from "./components/Services";
+import Experts from "./components/Experts";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <Navigation />
+      <Hero />
+      <div className="wrapper">
+        <Services />
+        <Experts />
+        <Contact />
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default App;
