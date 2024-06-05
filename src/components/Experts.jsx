2dbf965af09935550f816f29bffaaf44a4b65175
Sacha Arseneault <sacha.arseneault@gmail.com>
2024-06-04 21:03:33 -0400

    feat: blog d'experts

--- src/components/Experts.jsx
+++ src/components/Experts.jsx
@@ -15,6 +15,20 @@ export default function Experts() {
         "15 ans d’expérience culinaire. Vous pouvez choisir le menu ou laisser le chef décider pour vous. Vous pouvez aussi demander des plats spécifiques pour des régimes alimentaires particuliers.",
       image: expert1,
       price: "100$/h",
+      blog: [
+        {
+          title: "Comment préparer un bon repas",
+          content: "Voici les étapes pour préparer un bon repas...",
+        },
+        {
+          title: "Les secrets de la cuisine",
+          content: "Voici les secrets de la cuisine...",
+        },
+        {
+          title: "Les différents types de cuisines",
+          content: "Voici les différents types de cuisines...",
+        },
+      ],
     },
     {
       name: "Gordon",
@@ -23,6 +37,20 @@ export default function Experts() {
         "Reconnu mondialement pour ses talents. Vous pouvez choisir le menu ou laisser le chef décider pour vous. Vous pouvez aussi demander des plats spécifiques pour des régimes alimentaires particuliers.",
       image: expert2,
       price: "200$/h",
+      blog: [
+        {
+          title: "Comment préparer un bon repas",
+          content: "Voici les étapes pour préparer un bon repas...",
+        },
+        {
+          title: "Les secrets de la cuisine",
+          content: "Voici les secrets de la cuisine...",
+        },
+        {
+          title: "Les différents types de cuisines",
+          content: "Voici les différents types de cuisines...",
+        },
+      ],
     },
     {
       name: "Raj",
@@ -31,18 +59,32 @@ export default function Experts() {
         "Mangera la moitié de votre repas. Vous pouvez choisir le menu ou laisser le chef décider pour vous. Vous pouvez aussi demander des plats spécifiques pour des régimes alimentaires particuliers.",
       image: expert3,
       price: "50$/h",
+      blog: [
+        {
+          title: "Comment préparer un bon repas",
+          content: "Voici les étapes pour préparer un bon repas...",
+        },
+        {
+          title: "Les secrets de la cuisine",
+          content: "Voici les secrets de la cuisine...",
+        },
+        {
+          title: "Les différents types de cuisines",
+          content: "Voici les différents types de cuisines...",
+        },
+      ],
     },
   ];
 
   return (
     <section id="experts" className="py-5">
       <div className="container px-5">
-        <div className="row">
+        <div className="row px-5">
           <h2 className="display-4 fw-bolder text-white">Nos Experts</h2>
           <p className="lead text-white mb-5">
             Voici les experts qui prépareront vos repas
           </p>
-          <div className="col-lg-4 col-md-6">
+          <div className="col-lg-4 col-md-6 mt-3">
             <div className="card rounded-5">
               <img
                 src={experts[0].image}
@@ -64,7 +106,7 @@ export default function Experts() {
               </div>
             </div>
           </div>
-          <div className="col-lg-4 col-md-6">
+          <div className="col-lg-4 col-md-6 mt-3">
             <div className="card rounded-5">
               <img
                 src={experts[1].image}
@@ -86,7 +128,7 @@ export default function Experts() {
               </div>
             </div>
           </div>
-          <div className="col-lg-4 col-md-6">
+          <div className="col-lg-4 col-md-6 mt-3">
             <div className="card rounded-5">
               <img
                 src={experts[2].image}
@@ -135,6 +177,17 @@ export default function Experts() {
           <h2 className="mt-2">
             {modalIndex !== -1 && experts[modalIndex].price}
           </h2>
+          <h3 className="mt-5">Blog</h3>
+          {modalIndex !== -1 &&
+            experts[modalIndex].blog.map((blog, index) => (
+              <div key={index} className="card mt-2 p-3">
+                <h5>{blog.title}</h5>
+                <p>{blog.content}</p>
+                <button type="button" className="btn btn-primary w-25">
+                  Lire plus
+                </button>
+              </div>
+            ))}
         </Modal.Body>
         <Modal.Footer>
           <Button
